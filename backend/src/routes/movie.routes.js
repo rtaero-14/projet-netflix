510d0188dd06486e6a409ab26f63f78a767d1cb1
Reia TAERO <reiahoani.taero@etu.unilim.fr>
2026-03-25 14:04:41 +0100

    Modification des routes pour movie et rental après l ajout du middleware

--- backend/src/routes/movie.routes.js
+++ backend/src/routes/movie.routes.js
@@ -1,28 +1,22 @@
 import express from 'express';
-import {
-    getAllMovies,
-    getMovieById,
-    createMovie,
-    updateMovie,
-    deleteMovie,
-    getMovieStats,
-    getSimilarMovies
+import { 
+    getMovies, 
+    getMovie, 
+    createMovie, 
+    updateMovie, 
+    deleteMovie 
 } from '../controllers/movie.controller.js';
+import { protect, admin } from '../middleware/auth.middleware.js';
 
 const router = express.Router();
-// Routes publiques
-router.get('/', getAllMovies);
-router.get('/stats', getMovieStats);
-router.get('/:id', getMovieById);
-router.get('/:id/similar', getSimilarMovies);
-router.post('/', createMovie);
-router.put('/:id', updateMovie);
-router.delete('/:id', deleteMovie);
 
-// Routes protégées admin (sera activé séance 9)
-// router.post('/', protect, admin, createMovie);
-// router.put('/:id', protect, admin, updateMovie);
-// router.delete('/:id', protect, admin, deleteMovie);
-// Routes temporaires sans authentification (pour tester)
+// Routes publiques (Tout le monde peut voir les films)
+router.get('/', getMovies);
+router.get('/:id', getMovie);
+
+// Routes admin (Seul un admin connecté peut modifier le catalogue)
+router.post('/', protect, admin, createMovie);
+router.put('/:id', protect, admin, updateMovie);
+router.delete('/:id', protect, admin, deleteMovie);
 
 export default router;
--- backend/src/routes/rental.routes.js
+++ backend/src/routes/rental.routes.js
@@ -1,21 +1,18 @@
-import express from 'express'
-import {
-    createRental,
-    getMyRentals,
-    getAllRentals,
-    cancelRental,
-    getRentalStats,
-    getRecommendations
-} from '../controllers/rental.controller.js'
+import express from 'express';
+import { 
+    rentMovie, 
+    getMyRentals, 
+    getAllRentals 
+} from '../controllers/rental.controller.js';
+import { protect, admin } from '../middleware/auth.middleware.js';
 
 const router = express.Router();
 
-router.get('/', getAllRentals);
-router.get('/my-rentals', getMyRentals);
-router.get('/stats', getRentalStats);
-router.post('/', createRental);
-router.delete('/:id', cancelRental);
+router.use(protect);
 
-router.get('/recommendations', getRecommendations);
+router.get('/my', getMyRentals);
+router.post('/', rentMovie);
+
+router.get('/all', admin, getAllRentals);
 
 export default router;
